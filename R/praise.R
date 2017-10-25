8857f472a087f6b66425fa1f861dcd17548076d5
jesse-jesse <jessiel.r@gmail.com>
2017-10-25 14:21:09 +1000

    added some more priase to the lord

--- R/praise.R
+++ R/praise.R
@@ -3,6 +3,12 @@
 #'@description Sends love in the form of praise when in need.
 #'
 #' @export
+#'
+#' @examples
+#'
+#' # Praise the lord.
+#' praise()
+#'
 praise <- function() {
    print("Your're the best!")
 }
